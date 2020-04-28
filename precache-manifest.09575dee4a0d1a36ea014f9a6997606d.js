eb88593155fa8b285b43296cbc06ff57c859f2cb
roubzy1704 <roubzy1704@yahoo.com>
2020-04-28 17:58:04 -0500

    Updates

---
+++ precache-manifest.09575dee4a0d1a36ea014f9a6997606d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cbb744dd4ea3f3009db98408c31c1e00",
+    "url": "/Demographic-Predictor-App/index.html"
+  },
+  {
+    "revision": "5a97b2f30bc87bbe63de",
+    "url": "/Demographic-Predictor-App/static/css/main.fe4d164b.chunk.css"
+  },
+  {
+    "revision": "e0200f10f0bb481d310c",
+    "url": "/Demographic-Predictor-App/static/js/2.50a7a69c.chunk.js"
+  },
+  {
+    "revision": "8bb92c23487eb5c952e51992947cb95e",
+    "url": "/Demographic-Predictor-App/static/js/2.50a7a69c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5a97b2f30bc87bbe63de",
+    "url": "/Demographic-Predictor-App/static/js/main.afd3b644.chunk.js"
+  },
+  {
+    "revision": "ac1a37721fd15b63746e",
+    "url": "/Demographic-Predictor-App/static/js/runtime-main.f5c48448.js"
+  }
+]);
